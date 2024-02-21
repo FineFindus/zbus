8341359426d05ae16a3068b7585f835b10ccdd4f
FineFindus <FineFindus@proton.me>
2024-02-21 15:04:13 +0100

    wip: skip writing file

--- zbus_xmlgen/tests/gen.rs
+++ zbus_xmlgen/tests/gen.rs
@@ -1,24 +1,23 @@
 use pretty_assertions::assert_eq;
 use std::{env, error::Error, io::Write, path::Path};
 
-// use zbus_xml::Node;
-// use zbus_xmlgen::GenTrait;
+use zbus_xml::Node;
+use zbus_xmlgen::GenTrait;
 
 macro_rules! gen_diff {
     ($infile:literal, $outfile:literal) => {{
-        // let input = include_str!(concat!("data/", $infile));
+        let input = include_str!(concat!("data/", $infile));
         let expected = include_str!(concat!("data/", $outfile));
-        // #[cfg(windows)]
-        // let expected = expected.replace("\r\n", "\n");
-        // let node = Node::from_reader(input.as_bytes())?;
-        let gen = "ThisIsATestString".to_string();
-        // GenTrait {
-        //     interface: &node.interfaces()[0],
-        //     path: None,
-        //     service: None,
-        //     format: true,
-        // }
-        // .to_string();
+        #[cfg(windows)]
+        let expected = expected.replace("\r\n", "\n");
+        let node = Node::from_reader(input.as_bytes())?;
+        let gen = GenTrait {
+            interface: &node.interfaces()[0],
+            path: None,
+            service: None,
+            format: true,
+        }
+        .to_string();
 
         if env::var("TEST_OVERWRITE").is_ok() {
             let path = Path::new(env!("CARGO_MANIFEST_DIR"))
@@ -29,7 +28,7 @@ macro_rules! gen_diff {
                 .write(true)
                 .truncate(true)
                 .open(path)?;
-            f.write_all(gen.as_bytes())?;
+            // f.write_all(gen.as_bytes())?;
             f.flush()?;
             return Ok(());
         }
