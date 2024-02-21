182067d2f20261857b06e35206276d09ab974c73
FineFindus <FineFindus@proton.me>
2024-02-21 14:55:23 +0100

    wip: remove unused vars

--- zbus_xmlgen/tests/gen.rs
+++ zbus_xmlgen/tests/gen.rs
@@ -1,16 +1,16 @@
 use pretty_assertions::assert_eq;
 use std::{env, error::Error, io::Write, path::Path};
 
-use zbus_xml::Node;
-use zbus_xmlgen::GenTrait;
+// use zbus_xml::Node;
+// use zbus_xmlgen::GenTrait;
 
 macro_rules! gen_diff {
     ($infile:literal, $outfile:literal) => {{
-        let input = include_str!(concat!("data/", $infile));
+        // let input = include_str!(concat!("data/", $infile));
         let expected = include_str!(concat!("data/", $outfile));
         // #[cfg(windows)]
         // let expected = expected.replace("\r\n", "\n");
-        let node = Node::from_reader(input.as_bytes())?;
+        // let node = Node::from_reader(input.as_bytes())?;
         let gen = "ThisIsATestString".to_string();
         // GenTrait {
         //     interface: &node.interfaces()[0],
